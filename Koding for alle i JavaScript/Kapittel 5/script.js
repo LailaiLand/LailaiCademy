1218599741b18d2b8c44a204a9f11016f494b768
Nikolaipe <nikolaipe@getacademy.no>
2025-01-16 11:56:27 +0100

    Calculator working
    Heh, kanskje skulle indikert at jeg ikke lagde repo før kapittel 5...

--- Koding for alle i JavaScript/Kapittel 5/script.js
+++ Koding for alle i JavaScript/Kapittel 5/script.js
@@ -6,35 +6,43 @@ let num2 = 0;
 run();
 
 function run() {
-    console.log("testing at det kjører??");
   let addition = num1 + num2;
   let subtraction = num1 - num2;
   let multiplication = num1 * num2;
   let division = num1 / num2;
   let html = "";
 
-   html += /*HTML*/ `
+  html += /*HTML*/ `
         <h1>Kaaaaaalkulator!</h1>
         <p>Velg to tall og se resultatene!</p>
         <div class="choice">
             <div class="chosen">
-                <p>Tall 1</p>
-                <input class="number" type="range" min="0" max="100" value="${num1}" oninput="changeValue(this)" />
+                <p>Tall 1 = ${num1}</p>
+                <input class="number" type="range" min="0" max="10" value="${num1}" onchange="changeValue(this.value, 1)" />
             </div>
             <div class="chosen">
-                <p>Tall 2</p>
-                <input class="number" type="range" min="0" max="100" value="${num2}" oninput="changeValue(this)" />
+                <p>Tall 2 = ${num2}</p>
+                <input class="number" type="range" min="0" max="10" value="${num2}" onchange="changeValue(this.value, 2)" />
             </div>
         </div>
         <div class="result">
             ${num1} + ${num2} = ${addition} <br/>
             ${num1} - ${num2} = ${subtraction} <br/>
             ${num1} * ${num2} = ${multiplication} <br/>
-            ${num1} / ${num2} = ${division} <br/>
+            ${num1} / ${num2} = ${
+                checkIfZero(division) ? "kan ikke dele på 0" : division
+            } <br/>
         </div>
     `;
-    // console.log(app + "inne i run");
-    app.innerHTML = html;
+  app.innerHTML = html;
 }
-// console.log(app + "utenfor run");
-// console.log(document.body + "document");
+function checkIfZero(toCheck) {
+  return isNaN(parseInt(toCheck));
+}
+
+function changeValue(newValue, numNumber){
+  let changeTo = newValue;
+  if (numNumber == 1) num1 = changeTo;
+  else if (numNumber == 2) num2 = changeTo;
+  run();
+}
